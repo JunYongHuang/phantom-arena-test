b374d68bccd0a7ed9905a5a1bff3af0e21e5470f
hjy <545925654@qq.com>
2024-03-21 16:26:30 +0800

    222

--- web/js/Config-17b09.js
+++ web/js/Config-17b09.js
@@ -4,11 +4,11 @@
  * @Author: hjy
  * @Date: 2023-12-21 10:15:05
  * @LastEditors: hjy
- * @LastEditTime: 2024-03-21 03:10:23
+ * @LastEditTime: 2024-03-21 13:46:23
  */
 
 
-var game_version = "20240321_02";
+var game_version = "20240321_03";
 function getGameVersion(){
 	return game_version;
 }
