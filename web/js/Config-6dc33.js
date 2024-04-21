d93c994b6fb0882f388fdc4193df79f0748b0ba6
hjy <545925654@qq.com>
2024-04-21 21:07:59 +0800

    111

--- web/js/Config-6dc33.js
+++ web/js/Config-6dc33.js
@@ -4,11 +4,11 @@
  * @Author: hjy
  * @Date: 2023-12-21 10:15:05
  * @LastEditors: hjy
- * @LastEditTime: 2024-04-21 18:32:17
+ * @LastEditTime: 2024-04-21 21:03:13
  */
 
 
-var game_version = "20240421_02";
+var game_version = "20240421_03";
 
 var v_type = 1; //0=QA；1=S0; 2=SN;
 
