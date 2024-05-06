31e805cea91de5282dfc7133c3c682f0742debc1
hjy <545925654@qq.com>
2024-05-06 22:28:09 +0800

    11111

--- web/js/Config-bc40d.js
+++ web/js/Config-bc40d.js
@@ -4,11 +4,11 @@
  * @Author: hjy
  * @Date: 2023-12-21 10:15:05
  * @LastEditors: hjy
- * @LastEditTime: 2024-04-26 20:41:37
+ * @LastEditTime: 2024-05-06 22:15:33
  */
 
 
-var game_version = "20240429_04";
+var game_version = "20240506_01";
 
 var v_type = 1; //0=QA；1=S0; 2=SN;
 
