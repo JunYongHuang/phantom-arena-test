de24f7852f29f5c836f74893c094043bc28ac47b
hjy <545925654@qq.com>
2024-06-17 17:15:29 +0800

    1111

--- web/js/Config-4ec18.js
+++ web/js/Config-4ec18.js
@@ -4,11 +4,11 @@
  * @Author: hjy
  * @Date: 2023-12-21 10:15:05
  * @LastEditors: hjy
- * @LastEditTime: 2024-06-17 16:35:36
+ * @LastEditTime: 2024-06-17 17:06:48
  */
 
 
-var game_version = "V_0617_03";	
+var game_version = "V_0617_04";	
 
 var v_type = 1; //0=QA；1=S0; 2=SN;
 
