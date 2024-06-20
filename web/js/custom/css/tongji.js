fb58674c7b4f2e6cdb3b368904a8049845d890e2
hjy <545925654@qq.com>
2024-06-20 12:23:41 +0800

    1111

--- web/js/custom/css/tongji.js
+++ web/js/custom/css/tongji.js
@@ -8,19 +8,19 @@
  * @Description: 
  */
 
-var _hmt = _hmt || [];
-(function() {
-  var hm = document.createElement("script");
-  hm.src = "https://hm.baidu.com/hm.js?c400152bc72737aca6498bb206304940";
-  var s = document.getElementsByTagName("script")[0]; 
-  s.parentNode.insertBefore(hm, s);
-})();
+// var _hmt = _hmt || [];
+// (function() {
+//   var hm = document.createElement("script");
+//   hm.src = "https://hm.baidu.com/hm.js?c400152bc72737aca6498bb206304940";
+//   var s = document.getElementsByTagName("script")[0]; 
+//   s.parentNode.insertBefore(hm, s);
+// })();
 
 
 
-  // window.dataLayer = window.dataLayer || [];
-  // function gtag(){dataLayer.push(arguments);}
-  // gtag('js', new Date());
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
 
-  // gtag('config', 'G-B6F62P3M2K');
+  gtag('config', 'G-B6F62P3M2K');
 
