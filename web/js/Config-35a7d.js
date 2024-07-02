2d4dde7e42c7ad11db18768464747761611f3b75
hjy <545925654@qq.com>
2024-07-02 21:09:36 +0800

    11111

--- web/js/Config-35a7d.js
+++ web/js/Config-35a7d.js
@@ -4,11 +4,11 @@
  * @Author: hjy
  * @Date: 2023-12-21 10:15:05
  * @LastEditors: hjy
- * @LastEditTime: 2024-06-29 23:59:57
+ * @LastEditTime: 2024-07-02 21:05:17
  */
 
 
-var game_version = "V_0629_06";	
+var game_version = "V_0702_01";	
 
 var v_type = 1; //0=QA；1=S0; 2=SN;
 
