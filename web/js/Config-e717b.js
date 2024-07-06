5ab1dea740ad09372a88939099078f5fff95636f
hjy <545925654@qq.com>
2024-07-06 17:31:13 +0800

    1111

--- web/js/Config-e717b.js
+++ web/js/Config-e717b.js
@@ -4,11 +4,11 @@
  * @Author: hjy
  * @Date: 2023-12-21 10:15:05
  * @LastEditors: hjy
- * @LastEditTime: 2024-07-06 08:43:36
+ * @LastEditTime: 2024-07-06 17:11:17
  */
 
 
-var game_version = "V_0706_06";	
+var game_version = "V_0706_07";	
 
 var v_type = 1; //0=QA；1=S0; 2=SN;
 
