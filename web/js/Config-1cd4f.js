9505bcf045207f9b188a2f863c8c2b31650bb948
hjy <545925654@qq.com>
2024-07-15 15:05:55 +0800

    111

--- web/js/Config-1cd4f.js
+++ web/js/Config-1cd4f.js
@@ -8,7 +8,7 @@
  */
 
 
-var game_version = "V_0715_01";	
+var game_version = "V_0714_01";	
 
 var v_type = 1; //0=QA；1=S0; 2=SN;
 
@@ -164,8 +164,8 @@ function initLiveChat(){
 		//<!-- Start of LiveChat (www.livechat.com) code -->
 
 		window.__lc = window.__lc || {};
-		// window.__lc.license = 18239331;//正式版
-		window.__lc.license = 18239421;//测试版
+		window.__lc.license = 18239331;//正式版
+		// window.__lc.license = 18239421;//测试版
 		window.__lc.integration_name = "manual_onboarding";
 		window.__lc.product_name = "livechat";
 		;(function(n,t,c){function i(n){return e._h?e._h.apply(null,n):e._q.push(n)}var e={_q:[],_h:null,_v:"2.0",on:function(){i(["on",c.call(arguments)])},once:function(){i(["once",c.call(arguments)])},off:function(){i(["off",c.call(arguments)])},get:function(){if(!e._h)throw new Error("[LiveChatWidget] You can't use getters before load.");return i(["get",c.call(arguments)])},call:function(){i(["call",c.call(arguments)])},init:function(){var n=t.createElement("script");n.async=!0,n.type="text/javascript",n.src="https://cdn.livechatinc.com/tracking.js",t.head.appendChild(n)}};!n.__lc.asyncInit&&e.init(),n.LiveChatWidget=n.LiveChatWidget||e}(window,document,[].slice))
